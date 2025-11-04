c7e990a59157a3b82c1b9ac5b3a999552f8b5d7e
Madleyym <tryandaasu@gmail.com>
2025-11-04 21:35:16 +0700

    Cleanup Footer.tsx: comment out unused social media links for clarity

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -102,7 +102,7 @@ export default function Footer() {
                   />
                 </svg>
               </a>
-              <a
+              {/* <a
                 className="flex items-center gap-x-1.5 whitespace-nowrap text-sm font-medium text-neutral-500 hover:text-purple-600 transition"
                 href="https://twitter.com"
                 target="_blank"
@@ -121,7 +121,7 @@ export default function Footer() {
                     clipRule="evenodd"
                   />
                 </svg>
-              </a>
+              </a> */}
               {/* <a
                 className="flex items-center gap-x-1.5 whitespace-nowrap text-sm font-medium text-neutral-500 hover:text-purple-600 transition"
                 href="https://discord.com"
@@ -183,7 +183,7 @@ export default function Footer() {
                   />
                 </svg>
               </a> */}
-              <a
+              {/* <a
                 className="flex items-center gap-x-1.5 whitespace-nowrap text-sm font-medium text-neutral-500 hover:text-purple-600 transition"
                 href="https://base.org"
                 target="_blank"
@@ -202,7 +202,7 @@ export default function Footer() {
                     clipRule="evenodd"
                   />
                 </svg>
-              </a>
+              </a> */}
               {/* <a
                 className="flex items-center gap-x-1.5 whitespace-nowrap text-sm font-medium text-neutral-500 hover:text-purple-600 transition"
                 href="https://basescan.org"
@@ -259,7 +259,7 @@ export default function Footer() {
             </a>
 
             {/* Twitter */}
-            <a
+            {/* <a
               href="https://twitter.com"
               title="Twitter"
               target="_blank"
@@ -269,7 +269,7 @@ export default function Footer() {
               <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
               </svg>
-            </a>
+            </a> */}
 
             {/* Discord */}
             {/* <a
